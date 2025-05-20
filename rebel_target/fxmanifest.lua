7e2da4588826ddb1c01420895384a78fb86a1c5f
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-05-20 15:26:12 -0400

    added payment function
    added payment function to registers
    updated version to 1.0.0.6

---
+++ rebel_target/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "rebel_target"
+description "for qbcore"
+author "RebelGamer2k20"
+version "1.0.0.0"
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
